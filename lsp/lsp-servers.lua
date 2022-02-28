be4ddbde4f313db31e54d0d7be6f2f0efe37c641
kitkovsky <okitkowski114@gmail.com>
2022-02-28 18:21:00 +0100

    clean up lua formatting

--- lsp/lsp-servers.lua
+++ lsp/lsp-servers.lua
@@ -2,45 +2,46 @@
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-require'lspconfig'.tsserver.setup {}
-require'lspconfig'.clangd.setup {}
-require'lspconfig'.cmake.setup {}
-require'lspconfig'.sumneko_lua.setup {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-}
-require'lspconfig'.pyright.setup {}
-require'lspconfig'.html.setup {capabilities = capabilities}
-require'lspconfig'.cssls.setup {capabilities = capabilities}
-require'lspconfig'.jsonls.setup {
-    commands = {
-        Format = {
-            function()
-                vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line("$"), 0})
-            end
-        }
-    }
-}
+require("lspconfig").tsserver.setup({})
+require("lspconfig").clangd.setup({})
+require("lspconfig").cmake.setup({})
+require("lspconfig").sumneko_lua.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+require("lspconfig").pyright.setup({})
+require("lspconfig").html.setup({ capabilities = capabilities })
+require("lspconfig").cssls.setup({ capabilities = capabilities })
+require("lspconfig").jsonls.setup({
+	commands = {
+		Format = {
+			function()
+				vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+			end,
+		},
+	},
+})
 
-require"lsp_signature".setup()
+require("lsp_signature").setup()
 
 local cfg = {
-    floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
-    hint_enable = true, -- virtual hint enable
-    hint_prefix = "🐍 " -- parameter icon
+	floating_window = false, -- show hint in a floating window, set to false for virtual text only mode
+	hint_enable = true, -- virtual hint enable
+	hint_prefix = "🐍 ", -- parameter icon
 }
 
-require'lsp_signature'.on_attach(cfg)
+require("lsp_signature").on_attach(cfg)
 
-local nvim_lsp = require("lspconfig")
+-- local nvim_lsp = require("lspconfig")
 -- local servers = {
 -- "tsserver", "clangd", "cmake", "pyright", "html", "cssls", "jsonls"
 -- }
-local servers = {"tsserver", "clangd", "cmake", "pyright", "jsonls"}
-for _, lsp in ipairs(servers) do nvim_lsp[lsp].setup {on_attach = on_attach} end
-
+-- local servers = { "tsserver", "clangd", "cmake", "pyright", "jsonls" }
+-- for _, lsp in ipairs(servers) do
+-- 	nvim_lsp[lsp].setup({ on_attach = on_attach })
+-- end
