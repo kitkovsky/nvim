fdb7869dc53f1457dc318a6d3453ed15f1f8ff86
kitkovsky <okitkowski114@gmail.com>
2022-04-27 08:08:36 +0000

    linux specific config

--- lsp/cmp-config.lua
+++ lsp/cmp-config.lua
@@ -5,6 +5,8 @@ local cmp = require("cmp")
 
 cmp.setup({
 	mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-e>"] = cmp.mapping.close(),
@@ -19,15 +21,15 @@ cmp.setup({
 	sources = {
 		{ name = "nvim_lsp" },
 		{ name = "path" },
-		-- { name = "luasnip" },
 		{ name = "buffer" },
+		{ name = "luasnip" },
 	},
 
-	-- snippet = {
-	-- 	expand = function(args)
-	-- 		require("luasnip").lsp_expand(args.body)
-	-- 	end,
-	-- },
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
 
 	formatting = {
 		format = lspkind.cmp_format({
@@ -36,10 +38,8 @@ cmp.setup({
 				buffer = "[buf]",
 				nvim_lsp = "[LSP]",
 				path = "[path]",
-				-- luasnip = "[snip]",
+				luasnip = "[snip]",
 			},
 		}),
 	},
-
-	experimental = { native_menu = false, ghost_text = true },
 })
--- lsp/lsp-servers.lua
+++ lsp/lsp-servers.lua
@@ -1,6 +1,6 @@
 -- Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require("lspconfig").tsserver.setup({})
 -- require("lspconfig").clangd.setup({})
